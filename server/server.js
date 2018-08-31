9fffcdca00fdc59a309614a50d001d6c872d3925
unknown <hspreet1998@gmail.com>
2018-08-31 16:43:49 +0530

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -62,7 +62,12 @@ callback();
 
 socket.on('createMessage',(message,callback)=>{
 console.log('createMessage',message);
-io.emit('newMessage',generateMessage(message.from,message.text));
+var user=users.getUser(socket.id);
+
+if(user && isRealString(message.text)){
+    io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+}
+
 callback();
 });
 
@@ -75,7 +80,12 @@ callback();
 
 socket.on('createLocationMessage',(coords)=>{
 // io.emit('newMessage',generateMessage('Admin',coords.latitude+","+coords.longitude));
-io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+var user=users.getUser(socket.id);
+
+if(user){
+    io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+}
+
 });
 
 socket.on('disconnect',()=>{   // shows that user disconnected when close in chrome tab
